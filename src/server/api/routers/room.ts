3f260a8425078ec0d26e9a5a85f732be185a085d
Jayakarthik.jk <karthick72002@gmail.com>
2023-08-28 18:05:00 +0530

    changed roomRounter
    added support for bidding lesser than the highest bid
    added procedure for posting a bid without broadcasting

--- src/server/api/routers/room.ts
+++ src/server/api/routers/room.ts
@@ -1,5 +1,5 @@
 import { functionalityOptions } from "@/utils/validation";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { z } from "zod";
 
 export const roomRounter = createTRPCRouter({
@@ -39,4 +39,81 @@ export const roomRounter = createTRPCRouter({
         return new Error("Error fetching room");
       }
     }),
+  postABid: protectedProcedure
+    .input(
+      z.object({ roomId: z.string().cuid(), price: z.number().int().gt(0) })
+    )
+    .mutation(
+      async ({ input: { price, roomId }, ctx: { prisma, session } }) => {
+        const userId = session.user.id;
+        try {
+          return prisma.$transaction(async (prisma) => {
+            const room = await prisma.room.findUnique({
+              where: {
+                id: roomId,
+              },
+              include: {
+                highestBid: true,
+                product: true,
+              },
+            });
+            if (room === null) {
+              return new Error("Room not found");
+            }
+            if (room.product === null) {
+              return new Error(
+                "Product you are trying to bid was not found, try again later"
+              );
+            }
+            if (room.product.price >= price) {
+              return new Error("Bid amount too low");
+            }
+            if (room.product.sellerId === userId) {
+              return new Error("You cannot bid on your own product");
+            }
+            if (room.product.buyerId != null) {
+              return new Error("Product already sold");
+            }
+            // if (room.highestBid !== null && room.highestBid.price >= price) {
+            //   return new Error("Bid is too low");
+            // }
+            if (room.highestBid !== null) {
+              await prisma.bid.update({
+                where: {
+                  id: room.highestBid.id,
+                },
+                data: {
+                  highestBidedRoom: {
+                    disconnect: true,
+                  },
+                },
+              });
+            }
+            const bid = await prisma.bid.create({
+              data: {
+                price,
+                room: {
+                  connect: {
+                    id: roomId,
+                  },
+                },
+                user: {
+                  connect: {
+                    id: userId,
+                  },
+                },
+                highestBidedRoom: {
+                  connect: {
+                    id: roomId,
+                  },
+                },
+              },
+            });
+            return bid;
+          });
+        } catch (error) {
+          return new Error("cannot create bid");
+        }
+      }
+    ),
 });
