4335d344072a79eb67905cbadeb6e9857ba0090c
Naveen <imnaveenbharath@gmail.com>
2023-09-04 10:11:56 +0530

    Wishes component

---
+++ src/components/Wishes.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+// import { useRouter } from "next/router";
+import WishCard from "./WishCard";
+import { api } from "@/utils/api";
+import { useEffect } from "react";
+
+function Wishes() {
+  // const router = useRouter();
+  // const userId = router.query.userid as string;
+  const {
+    data: wishes,
+    isLoading,
+    refetch,
+  } = api.user.getMywishes.useQuery({});
+
+  useEffect(() => {
+    void refetch();
+  }, [refetch]);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (wishes instanceof Error) return <div>Something went wrong</div>;
+  if (!wishes || wishes.length === 0) return <div>No data to Show</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="mt-10 flex w-full flex-col items-center gap-5">
+      <div className="list w-full space-y-3">
+        {wishes.map((wish) => (
+          <WishCard key={wish.id} wish={wish} />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Wishes;
