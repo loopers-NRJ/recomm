788cbe2067090833e2597a8893e5959c95593a28
Jayakarthik-jk <karthick72002@gmail.com>
2023-09-18 08:33:51 +0530

    removed all migation and made one single migration

--- prisma/migrations/20230816145004_made_higest_bided_room_property_optional_in_bid_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Bid" DROP CONSTRAINT "Bid_highestBidedRoomId_fkey";
-
--- AlterTable
-ALTER TABLE "Bid" ALTER COLUMN "highestBidedRoomId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Bid" ADD CONSTRAINT "Bid_highestBidedRoomId_fkey" FOREIGN KEY ("highestBidedRoomId") REFERENCES "Room"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20230826145429_added_referential_action_ondelete_cascade_between_product_and_room/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Product" DROP CONSTRAINT "Product_roomId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Product" ADD CONSTRAINT "Product_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE CASCADE ON UPDATE CASCADE;
