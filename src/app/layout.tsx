c4c3b68c2c2fa1256d9183214a5ec0f705694073
Naveen <imnaveenbharath@gmail.com>
2024-01-09 15:16:20 +0530

    app-router-refactor/login-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,11 @@ import "@/styles/globals.css";
 
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
-// import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { TRPCReactProvider } from "@/trpc/react";
 import Providers from "./providers";
 import { type Metadata } from "next";
 import Navbar from "@/components/navbar/Navbar";
-// import BottomBar from "@/components/navbar/BottomBar";
-// import LocationUpdater from "@/components/common/LocationUpdater";
-// import TopLoader from "@/components/loading/TopLoader";
-// import { Toaster } from "@/components/ui/toaster";
-// import ModalRenderer from "@/components/modals/core/modal-renderer";
 
 const inter = Inter({
   subsets: ["latin"],
