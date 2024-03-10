a9a9d3933022addd7b110ac93611ea046adada18
Naveen <imnaveenbharath@gmail.com>
2024-03-10 19:22:27 +0530

    fix: typo in the Product Type

--- src/app/favourites/products.tsx
+++ src/app/favourites/products.tsx
@@ -4,7 +4,9 @@ import type { ProductsPayloadIncluded } from '@/types/prisma';
 import { motion } from 'framer-motion';
 import React from 'react'
 
-function Favourites({ products }: { products: ProductsPayloadIncluded[] }) {
+type Product = ProductsPayloadIncluded & { isFavorite?: boolean };
+
+function Favourites({ products }: { products: Product[] }) {
   return (
     <motion.div className="product-area mb-32 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6"
       variants={{
