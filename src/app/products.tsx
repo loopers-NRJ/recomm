181a0d34381ecab3810e589fc2c05bb080d762f6
Naveen <imnaveenbharath@gmail.com>
2024-03-16 10:29:35 +0530

    feat: Ad Section in the homepage

--- src/app/products.tsx
+++ src/app/products.tsx
@@ -2,11 +2,14 @@
 
 import ListingCard from "@/components/ListingCard"
 import LoadingProducts from "@/components/loading/LoadingProducts";
+import Autoplay from "embla-carousel-autoplay"
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import Image from "next/image";
 import { useClientSelectedState } from "@/store/SelectedState";
 import { api } from "@/trpc/react";
 import { motion } from "framer-motion"
 
-function Products() {
+export default function Products() {
   const state = useClientSelectedState((selected) => selected.state)
   const productQuery = api.product.all.useQuery({ state })
 
@@ -27,14 +30,33 @@ function Products() {
       animate="show"
       className="product-area mb-32 grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6"
     >
-      {productQuery.data?.products.map((product) => (
-        <ListingCard
-          key={product.id}
-          product={product}
-        />
-      ))}
+      {productQuery.data?.products.map((product, i) => {
+        if (i === 4) {
+          return <AdCard key={i} />
+        }
+        return <ListingCard key={product.id} product={product} />
+      })}
     </motion.div>
   )
 }
 
-export default Products
+export function AdCard() {
+  return (
+    <Carousel
+      opts={{loop: true}}
+      plugins={[Autoplay({ delay: 3000 })]}
+      className="col-span-2 rounded-xl overflow-hidden">
+      <CarouselContent>
+        <CarouselItem className="rounded-xl overflow-hidden">
+          <Image width={400} height={100} src="/ad/mobile.jpg" alt="ad1" className="object-cover" />
+        </CarouselItem>
+        <CarouselItem className="rounded-xl overflow-hidden">
+          <Image width={400} height={100} src="/ad/bike.jpg" alt="ad1" className="object-cover" />
+        </CarouselItem>
+        <CarouselItem className="rounded-xl overflow-hidden">
+          <Image width={400} height={100} src="/ad/car.jpg" alt="ad1" className="object-cover"/>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  )
+}
