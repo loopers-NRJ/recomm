242362f3fa163420863ca8f4746bc71633b71ff9
loopers-NRJ <124859008+loopers-NRJ@users.noreply.github.com>
2024-03-17 18:23:11 +0000

    feat: added user location retriever component

---
+++ src/store/ClientLocation.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+type LocationState = {
+  coords?: { latitude: number; longitude: number };
+};
+type LocationAction = { setLocation: (state: LocationState["coords"]) => void };
+
+export const useClientLocation = create<LocationState & LocationAction>(
+  (set) => ({
+    setLocation: (state) => set({ coords: state }),
+  }),
+);
